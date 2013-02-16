b31461268745b9337dd886d103c84c8a97644368
John Wulff <johnw@orcasnet.com>
2013-02-16 13:40:38 -0800

    Generate Rails app
    Generated Rails 3.2.12 app with command `rails new twuzz -T -d sqlite3`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twuzz::Application.config.secret_token = '9c5c355148159f0d82bd82df227e56e68005d05f50d24abbfb55108276defefcf0acbb5ab807e8f22323a38de7270833240ae8104ab116a7472242ec1f0ae17d'
